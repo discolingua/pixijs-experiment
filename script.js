1491ac90d2a122675a478d1cdf44a8f238ad533a
Bunny Blake <sonicforest@gmail.com>
2021-01-31 16:06:28 -0500

    simple tilemap displays

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const app = new PIXI.Application({
+// PIXI.js aliases
+let Application = PIXI.Application,
+	Container = PIXI.Container,
+	Sprite = PIXI.Sprite;
+
+
+const app = new Application({
 	width: 800,
 	height: 600,
 	resolution: window.devicePixelRatio,
 	autoDensity: true
 });
 
-document.body.appendChild(app.view);
+const tileSize = 16;
 
-// const tileSize = 16;
+let map = {
+	width: 4,
+	height: 4,
+	tiles: [ 0,0,0,0,
+			 0,0,0,0,
+			 0,0,0,0,
+			 0,0,0,0]
+}
+
+
+document.body.appendChild(app.view);
 
 
 
@@ -23,7 +39,30 @@ function handleLoadError() {
 }
 
 function handleLoadComplete() {
-	const img = new PIXI.Sprite(loader.resources.tileset.texture);
-	app.stage.addChild(img);
-}
 
+	// slice up tile textures from sprite sheet
+	let tileTextures = [];
+	for (let i = 0; i < 77; i++) {
+		let x = i % 7;
+		let y = Math.floor(i / 7);
+		tileTextures[i] = new PIXI.Texture(
+			loader.resources.tileset.texture,
+			new PIXI.Rectangle(x * tileSize, y * tileSize, tileSize, tileSize));
+	}
+
+	const img = new Sprite(tileTextures[55]);
+
+	// build background map from map object
+	let background = new Container();
+	for (let y = 0; y < map.height; y++) {
+		for (let x = 0; x < map.width; x++) {
+			let tile = map.tiles[y * map.width + x];
+			let sprite = new Sprite(tileTextures[tile]);
+			sprite.x = x * tileSize;
+			sprite.y = y * tileSize;
+			background.addChild(sprite);
+		}
+	}
+
+	app.stage.addChild(background);
+}
